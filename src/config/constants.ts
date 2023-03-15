43fafc7c970592ea5f0a530ce1cb3bd574edd860
Akiko <108007568+avdalmi@users.noreply.github.com>
2023-03-15 17:23:39 +0100

    deploy

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,3 @@
 export const apiUrl: string =
-  process.env.REACT_API_URL || "https://japaneasy.herokuapp.com/";
+  process.env.REACT_API_URL || "http://localhost:4000";
 export const DEFAULT_MESSAGE_TIMEOUT: number = 2000;
